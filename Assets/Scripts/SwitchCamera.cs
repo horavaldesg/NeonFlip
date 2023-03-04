88aa3d47e42be5b125e0e0c3eaffe0b8362e9a06
Horacio Valdes <horavaldesg@gmail.com>
2023-03-04 17:53:04 -0500

    Started Making Ground Transparent

--- Assets/Scripts/SwitchCamera.cs
+++ Assets/Scripts/SwitchCamera.cs
@@ -8,27 +8,47 @@ public class SwitchCamera : MonoBehaviour
     public static event Action<bool> ChangePerspective;
     public GameObject camera1;
     public GameObject camera2;
+    [SerializeField] private LayerMask layerMask;
     public static bool space;
     GameObject[] cameras;
     [SerializeField] bool spacePub;
+    [SerializeField] private Material solidGround;
+    [SerializeField] private Material transparentGround;
+    private MeshRenderer _currentMeshRenderer;
+    
+    private Camera _currentCameraSelected;
+    private Camera cam1;
+    private Camera cam2;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
         camera1 = GameObject.FindGameObjectWithTag("MainCamera");
         camera2 = GameObject.FindGameObjectWithTag("MainCamera2");
-        camera2.SetActive(false);
+        camera1.TryGetComponent(out cam1);
+        camera2.TryGetComponent(out cam2);
+        
+        Switch();
         spacePub = true;
         space = spacePub;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Space) && space)
-        //{
-        //    Switch();
-        //}
+        if (Physics.Raycast(_currentCameraSelected.transform.position, _currentCameraSelected.transform.forward,
+                out var hit, 100.0f, layerMask))
+        {
+            if (layerMask == LayerMask.NameToLayer("Player")) return;
+            hit.collider.gameObject.TryGetComponent(out _currentMeshRenderer);
+            _currentMeshRenderer.material = transparentGround;
+            Debug.Log(hit.collider.gameObject.name + "Collided With Ground");
+        }
+        else
+        {
+            if(!_currentMeshRenderer) return;
+            _currentMeshRenderer.material = solidGround;
+        }
     }
 
     public void Switch()
@@ -39,6 +59,7 @@ public class SwitchCamera : MonoBehaviour
             camera2.SetActive(true);
             camera1.SetActive(false);
             PlayerController.SideView = false;
+            SetCurrentCameraTransform(cam2);
             ChangePerspective?.Invoke(PlayerController.SideView);
         }
         //Side View
@@ -47,6 +68,7 @@ public class SwitchCamera : MonoBehaviour
             camera1.SetActive(true);
             camera2.SetActive(false);
             PlayerController.SideView = true;
+            SetCurrentCameraTransform(cam1);
             ChangePerspective?.Invoke(PlayerController.SideView);
         }
     }
@@ -54,5 +76,10 @@ public class SwitchCamera : MonoBehaviour
     public static void CanUseSpace()
     {
         space = !space;
+    }
+
+    private void SetCurrentCameraTransform(Camera cameraTransform)
+    {
+        _currentCameraSelected = cameraTransform;
     }
 }
